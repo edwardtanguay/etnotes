678a0e17a52981204cbae3499d505a5c0cc7f473
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-16 13:55:01 +0200

    one howto

--- src/models/model.ts
+++ src/models/model.ts
@@ -1,9 +1,3 @@
-import { buildForays } from './forayModel';
 import { buildHowtos } from './howtoModel';
-import { buildFrontendStarters } from './starterModel';
-import { buildFrontendStarterTechnologies } from './starterTechnologyModel';
 
 export const howtos = buildHowtos();
-export const forays = buildForays();
-export const frontendStarters = buildFrontendStarters();
-export const frontendStarterTechnologies = buildFrontendStarterTechnologies();
