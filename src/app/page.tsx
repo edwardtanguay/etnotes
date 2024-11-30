47410a2bb3bdbf512ee021cd72ec137c89030599
Edward Tanguay (windows) <edward@tanguay.info>
2024-11-30 03:37:29 +0100

    hamb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,6 +16,10 @@ export default function Home() {
 				<li><a href="http://etnotes.vercel.app/maps/denmark.kmz">Copenhagen map</a></li>
 				<li><a href="https://www.airbnb.de/s/guidebooks?refinement_paths%5B%5D=%2Fguidebooks%2F2150833&share_channel=copy&_set_bev_on_new_domain=1714656467_MDNkYWVjMzE0YTY4">airbnb book places</a></li>
 			</ul>
+			<h3 className="text-xl mb-3 text-yellow-100">Hamburg Trip 2024</h3>
+			<ul className="list-disc ml-4 mb-5">
+				<li><a href="http://etnotes.vercel.app/maps/hamburg2024.kmz">Hamburg map</a></li>
+			</ul>
 		</main>
 	);
 }
